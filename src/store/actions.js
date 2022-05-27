845482d3bbd6bd9043446745bee4d610118c937a
jatortuc <jatort@uc.cl>
2022-05-27 19:10:24 -0400

    login ready

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from 'axios';
 
 export const Init = () => {
   return async dispatch => {
@@ -13,21 +14,42 @@ export const Init = () => {
   }
 }
 
-export const Login = (username, password) => {
+export const Login = (email, password) => {
   return async dispatch => {
     let token = null;
-    if (username === 'vishal' && password == '1234') {
-      token = username + password;
-      // here we can use login api to get token and then store it
+    try {
+      const response = await axios.post('https://entradapp-backend.herokuapp.com/api/v1/login', {
+        email,
+        password,
+      })
+      const {
+        data: { token },
+      } = response
       await AsyncStorage.setItem('token', token);
-      console.log('token stored');
+      dispatch({
+        type: 'LOGIN',
+        payload: token,
+      })
+    } catch (error) {
+      console.log(error)
     }
-    dispatch({
-      type: 'LOGIN',
-      payload: token,
-    })
   }
 }
+// export const Login = (username, password) => {
+//   return async dispatch => {
+//     let token = null;
+//     if (username === 'vishal' && password == '1234') {
+//       token = username + password;
+//       // here we can use login api to get token and then store it
+//       await AsyncStorage.setItem('token', token);
+//       console.log('token stored');
+//     }
+//     dispatch({
+//       type: 'LOGIN',
+//       payload: token,
+//     })
+//   }
+// }
 
 export const Logout = () => {
   return async dispatch => {
