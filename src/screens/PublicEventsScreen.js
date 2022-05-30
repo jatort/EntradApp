23edd67a13727a93239b7ae339dcf74bd03cdac8
elias <elias.sabja@uc.cl>
2022-05-30 13:12:32 -0400

    Fetching events from API

--- src/screens/PublicEventsScreen.js
+++ src/screens/PublicEventsScreen.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import { Button } from 'react-native-paper'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native'
 import { useDispatch } from 'react-redux';
 import { Logout } from '../store/actions';
 import Colors from '../constants/Colors';
 import EventList from '../shared/EventList';
+import { getAllEvents } from '../lib/event';
+import { Event } from '../types/event';
 
 export default function PublicEventsScreen({ route, navigation }) {
+  const [events, setEvents] = useState([]);
 
   const dispatch = useDispatch();
   const submit = () => {
     dispatch(Logout())
   }
+
+  const handleOnClick = async () =>{
+    console.log("Clicking!");
+    const events = await getAllEvents();
+    console.log("Eventos:");
+    console.log(events);
+    setEvents(events);
+  }
+  /*useEffect(async() => {
+    const events = await getAllEvents();
+    console.log("Use effect");
+  }, []);*/
+
   return (
     <View style={styles.root}>
-      <EventList title={"Eventos públicos"}/>
+      <EventList title={"Eventos públicos"} events={events}/>
+      <Button onPress={() => handleOnClick()} title="Eventos" />
+
     </View>
+
   )
 }
 
--- src/shared/EventList.js
+++ src/shared/EventList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, ScrollView, Text } from 'react-native';
 import EventCard from './EventCard';
 import Colors from '../constants/Colors';
@@ -13,15 +13,28 @@ type Props = {
 // Este componente debe hacer fetch de la API para obtener
 // los eventos en distintas url segun la pantalla
 const EventList: React.FC<Props> = (props: Props) => {
+  const [ isLoading, setIsLoading ] = useState(true);
+
+  useEffect(() => {
+    console.log("Rendering events");
+    console.log(props.events);
+    if (props.events.length > 0) setIsLoading(false);
+  }, [props.events]);
+
   return (
    <View style={styles.root}>
     <ScrollView>
-      <Text style={styles.title}>{props.title}</Text>
+    { isLoading ? 
+      <View style={styles.root}> 
+      <Text style={styles.title}>{props.title} Loading</Text>
       <EventCard />
       <EventCard />
       <EventCard />
       <EventCard />
       <EventCard />
+      </View>:
+      <View style={styles.root}><Text style={styles.title}>{props.title} Loaded</Text></View>
+    }
     </ScrollView>
   </View> 
   );
