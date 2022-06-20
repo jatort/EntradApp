1b116c2aaa3ab4c5791c62cd304b61495f5a0d53
Elias <elias.sabja@uc.cl>
2022-06-19 21:54:01 -0400

    Connected my tickets with backend

--- src/lib/ticket.js
+++ src/lib/ticket.js
@@ -1,11 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from 'axios';
 import { config } from '../config'
-import { response } from '../mock_api/tickets';
 
 const url = () => config.API_URL;
 
 export const getMyTickets = async() => {
   try {
+    const token = await AsyncStorage.getItem("token");
+    const response = await axios.get(`${url()}/ticket`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return getValidTickets(response.data.tickets);
   } catch (err) {
     console.log(err);
