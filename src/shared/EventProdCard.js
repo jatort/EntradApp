deea514c7cf6fd3ea930fac0030e46942b324fdd
julio <jnandrade@uc.cl>
2022-06-20 20:19:54 -0400

    logs only with message

--- src/shared/EventProdCard.js
+++ src/shared/EventProdCard.js
@@ -1,19 +1,16 @@
-import React, { useState, useEffect} from 'react'
-import { getUser } from '../lib/user';
+import React, { useState, useEffect } from "react";
+import { getUser } from "../lib/user";
 
-import {
-  View, Text, Image
-} from 'react-native'
+import { View, Text, Image } from "react-native";
 
 const ProdCard = (props) => {
-
   // Acá debo hacer la consulta por el usuario
 
-  const [event, setEvent] = useState({})
-  const [user, setUser] = useState({})
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [role, setRole] = useState('');
+  const [event, setEvent] = useState({});
+  const [user, setUser] = useState({});
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
 
   useEffect(() => {
     setEvent(props.event);
@@ -24,22 +21,21 @@ const ProdCard = (props) => {
         setRole(user.role);
         setEmail(user.email);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err.message));
   }, [props.event]);
 
   const imgUrl = user
-    ? require('../../assets/default-user.png')
-    : require('../../assets/default-user.png')
-
+    ? require("../../assets/default-user.png")
+    : require("../../assets/default-user.png");
 
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: 'column' }}>
+      <View style={{ flexDirection: "column" }}>
         <View style={styles.imageContainer}>
           <Image style={styles.prodImage} source={imgUrl} size={35} />
         </View>
       </View>
-      <View style={{ flexDirection: 'column' }}>
+      <View style={{ flexDirection: "column" }}>
         <View style={styles.nameContainer}>
           <Text style={styles.name}>{username}</Text>
         </View>
@@ -48,12 +44,12 @@ const ProdCard = (props) => {
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = {
   container: {
-    flexDirection: 'row',
+    flexDirection: "row",
     borderRadius: 8,
     paddingHorizontal: 15,
     width: 325,
@@ -61,36 +57,36 @@ const styles = {
     marginVertical: 2,
   },
   imageContainer: {
-    justifyContent: 'flex-start',
-    backgroundColor: '#E5E5E5',
+    justifyContent: "flex-start",
+    backgroundColor: "#E5E5E5",
     borderRadius: 14,
-    color: '#414abe',
+    color: "#414abe",
     marginRight: 10,
     marginVertical: 2,
     maxWidth: 46,
     maxHeight: 46,
   },
   prodImage: {
-    maxWidth: '100%',
-    maxHeight: '100%',
+    maxWidth: "100%",
+    maxHeight: "100%",
   },
   nameContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginVertical: 5,
   },
   name: {
-    justifyContent: 'flex-start',
-    color: 'black',
+    justifyContent: "flex-start",
+    color: "black",
     fontSize: 15,
   },
   roleContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   role: {
-    justifyContent: 'flex-start',
-    color: '#747688',
+    justifyContent: "flex-start",
+    color: "#747688",
     fontSize: 10,
   },
-}
+};
 
-export default ProdCard
+export default ProdCard;
