251e0208a563e099d24f01c2b3372fb8415f0bb7
Elias <elias.sabja@uc.cl>
2022-06-22 23:59:14 -0400

    Fix config url

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,3 @@
 export const config = {
-  // API_URL: 'https://entradapp-backend.herokuapp.com/api/v1'
-  API_URL: 'http://192.168.100.9:8000/api/v1'
+  API_URL: 'https://entradapp-backend.herokuapp.com/api/v1'
 }
